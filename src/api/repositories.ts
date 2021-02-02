e4ad44994fc56ca9caefa084a38766e5a914ea1f
Alan Daniel <stylesshjs@gmail.com>
2021-02-02 20:59:43 +0000

    feat: languagues colors added to repositiories.

--- src/api/repositories.ts
+++ src/api/repositories.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const getRepositories = async (): Promise<void> => {
-  const { data } = await axios.get(
-    "https://api.github.com/users/stylessh/repos?per_page=50"
-  );
+export const getRepositories = async (): Promise<any> => {
+    const { data } = await axios.get(
+        "https://api.github.com/users/stylessh/repos?per_page=50"
+    );
 
-  return data;
+    return data;
 };
--- src/components/Repositories/index.tsx
+++ src/components/Repositories/index.tsx
@@ -1,48 +1,57 @@
 import React, { useEffect, useState } from "react";
+
 import { getRepositories } from "@Api/repositories";
 import { FaStar, FaCodeBranch } from "react-icons/fa";
+import langColors from '@Utils/getLangColor';
 
 const Repositories = () => {
-  const [repos, setRepos] = useState<any>([]);
+    const [repos, setRepos] = useState<any>([]);
 
-  useEffect(() => {
-    const getData = async () => {
-      const repos = await getRepositories();
+    useEffect(() => {
+        const getData = async () => {
+            const repos = await getRepositories();
 
-      setRepos(repos);
-    };
+            console.log(repos[0])
 
-    getData();
-  }, []);
+            setRepos(repos);
+        };
 
-  return (
-    <div className="repositories">
-      {repos &&
-        repos.map((repo) => (
-          <a href={repo.svn_url} target="_blank" className="repo" key={repo.id}>
-            <div className="info">
-              <h3 className="name">{repo.name}</h3>
+        getData();
+    }, []);
 
-              <p className="description">{repo.description}</p>
-            </div>
 
-            <div className="stats">
-              <div className="counts">
-                <div className="stars">
-                  <FaStar /> {repo.stargazers_count}
-                </div>
+    useEffect(() => {
 
-                <div className="forks">
-                  <FaCodeBranch /> {repo.forks}
-                </div>
-              </div>
+    }, [])
 
-              <h4 className="lang">{repo.language}</h4>
-            </div>
-          </a>
-        ))}
-    </div>
-  );
+    return (
+        <div className="repositories">
+            {repos &&
+                repos.map((repo) => (
+                    <a href={repo.svn_url} target="_blank" className="repo" key={repo.id}>
+                        <div className="info">
+                            <h3 className="name">{repo.name}</h3>
+
+                            <p className="description">{repo.description}</p>
+                        </div>
+
+                        <div className="stats">
+                            <div className="counts">
+                                <div className="stars">
+                                    <FaStar /> {repo.stargazers_count}
+                                </div>
+
+                                <div className="forks">
+                                    <FaCodeBranch /> {repo.forks}
+                                </div>
+                            </div>
+
+                            <h4 className="lang"> <span className="color" style={{ background: langColors[repo.language] }}></span> {repo.language}</h4>
+                        </div>
+                    </a>
+                ))}
+        </div>
+    );
 };
 
 export default Repositories;
